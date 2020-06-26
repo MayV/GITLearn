afef544cd58ba0e60e484b9c9e79ad52dce995eb
Mayur Vaid <mayur26vaid@gmail.com>
2020-06-26 15:17:29 +0530

    Changes made locally

--- SourceCode/exp/Hello.c
+++ SourceCode/exp/Hello.c
@@ -5,6 +5,7 @@ void HelloWorld() {
 }
 
 void greetingFunctionStub() {
+	printf("Greetings from a developer!")
 }
 
 int main() {
--- SourceCode/math/Arithmatic.c
+++ SourceCode/math/Arithmatic.c
@@ -6,7 +6,10 @@ int sub(int x, int y) {
 	return x-y;
 }
 
-int mul(int x, int) {
+int mul(int x, int y) {
 	return x*y;
 }
 
+int div(int x, int y) {
+	return x/y;
+}
